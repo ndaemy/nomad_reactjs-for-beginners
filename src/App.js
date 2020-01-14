8b1d29562d1b95561259b852ae7d9c45baaac6b2
yuyaebean <yuyaebean@gmail.com>
2020-01-15 01:01:39 +0900

    2.2 React Router part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from 'components/Header';
 import Router from 'components/Router';
 
 export default () => (
   <>
+    <Header />
     <Router />
   </>
 );
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 import Home from 'routes/Home';
 import TV from 'routes/TV';
@@ -7,8 +12,11 @@ import Search from 'routes/Search';
 
 export default () => (
   <Router>
-    <Route path='/' exact component={Home} />
-    <Route path='/tv' component={TV} />
-    <Route path='/search' component={Search} />
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/tv' component={TV} />
+      <Route path='/search' component={Search} />
+      <Redirect from='*' to='/' />
+    </Switch>
   </Router>
 );
