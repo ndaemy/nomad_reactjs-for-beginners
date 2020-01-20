7c3d882aaef344dc0aa0ab14be0b72709588c1af
yuyaebean <yuyaebean@gmail.com>
2020-01-20 23:30:30 +0900

    5.5 Detail Container part 1

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import Header from './Header';
-import Movie from 'routes/Movie';
-import TV from 'routes/TV';
-import Search from 'routes/Search';
+import Header from "./Header";
+import Movie from "routes/Movie";
+import TV from "routes/TV";
+import Search from "routes/Search";
+import Detail from "routes/Detail";
 
 export default () => (
   <Router>
     <Header />
     <Switch>
-      <Route path='/' exact component={Movie} />
-      <Route path='/tv' component={TV} />
-      <Route path='/search' component={Search} />
-      <Redirect from='*' to='/' />
+      <Route path="/" exact component={Movie} />
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
+      <Route path="/movie/:id" component={Detail} />
+      <Route path="/tv/:id" component={Detail} />
+      <Redirect from="*" to="/" />
     </Switch>
   </Router>
 );
--- src/routes/Detail/DetailContainer.js
+++ src/routes/Detail/DetailContainer.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import DetailPresenter from './DetailPresenter';
+import DetailPresenter from "./DetailPresenter";
 
-export default () => {
+export default props => {
   const [result, setResult] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = props;
+
+    const parsedInt = parseInt(id);
+    if (isNaN(parsedInt)) {
+      return push("/");
+    }
+  }, []);
 
   return <DetailPresenter result={result} error={error} loading={loading} />;
 };
