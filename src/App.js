a8a141b8aab73d8fb52a1702ec600f51cb898b2a
yuyaebean <yuyaebean@gmail.com>
2020-04-10 02:54:05 +0900

    3.3 GlobalStyles and Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Router from 'components/Router';
+import GlobalStyles from 'components/GlobalStyles';
 
 function App() {
   return (
     <>
       <Router />
+      <GlobalStyles />
     </>
   );
 }
