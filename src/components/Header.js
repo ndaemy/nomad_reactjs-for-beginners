99a5b6e16c1fc91fea092d06d8f458d597f7ad24
yuyaebean <yuyaebean@gmail.com>
2020-04-10 03:05:17 +0900

    3.4 Location Aware Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 const SHeader = styled.header`
@@ -22,9 +22,12 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-  width: 50px;
+  width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 3px solid
+    ${props => (props.current ? '#3498db' : 'transparent')};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -35,16 +38,18 @@ const SLink = styled(Link)`
 `;
 
 export default function Header() {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <SHeader>
       <List>
-        <Item>
+        <Item current={pathname === '/'}>
           <SLink to='/'>Movies</SLink>
         </Item>
-        <Item>
+        <Item current={pathname === '/tv'}>
           <SLink to='/tv'>TV</SLink>
         </Item>
-        <Item>
+        <Item current={pathname === '/search'}>
           <SLink to='/search'>Search</SLink>
         </Item>
       </List>
