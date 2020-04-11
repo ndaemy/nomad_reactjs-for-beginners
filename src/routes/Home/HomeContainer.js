4102ec11623155fe780365304c6b5a321b11fc15
yuyaebean <yuyaebean@gmail.com>
2020-04-12 00:34:58 +0900

    5.2 Home Container

--- src/routes/Home/HomeContainer.js
+++ src/routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HomePresenter from './HomePresenter';
+import { movieApi } from 'api';
 
 export default class HomeContainer extends Component {
   state = {
@@ -9,6 +10,33 @@ export default class HomeContainer extends Component {
     loading: true,
     error: null,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await movieApi.nowPlaying();
+      const {
+        data: { results: upcoming },
+      } = await movieApi.upcoming();
+      const {
+        data: { results: popular },
+      } = await movieApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find movies information.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
     const { nowPlaying, upcoming, popular, loading, error } = this.state;
