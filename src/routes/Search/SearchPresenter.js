7a8ab7b8e81fd5473260088653c5c73776dc0ae7
yuyaebean <yuyaebean@gmail.com>
2020-04-15 04:48:22 +0900

    6.6 Render Poster component

--- src/routes/Search/SearchPresenter.js
+++ src/routes/Search/SearchPresenter.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 import Loader from 'components/Loader';
 import Section from 'components/Section';
+import Poster from 'components/Poster';
 import Message from 'components/Message';
 
 const Container = styled.div`
@@ -45,14 +46,33 @@ export default function SearchPresenter({
           {movieResults && movieResults.length > 0 && (
             <Section title='Movie Results'>
               {movieResults.map(movie => (
-                <span key={movie.id}>{movie.title}</span>
+                <Poster
+                  key={movie.id}
+                  id={movie.id}
+                  imageUrl={movie.poster_path}
+                  title={movie.original_title}
+                  rating={movie.vote_average}
+                  year={
+                    movie.release_date && movie.release_date.substring(0, 4)
+                  }
+                  isMovie={true}
+                />
               ))}
             </Section>
           )}
           {tvResults && tvResults.length > 0 && (
             <Section title='TV Show Results'>
               {tvResults.map(show => (
-                <span key={show.id}>{show.name}</span>
+                <Poster
+                  key={show.id}
+                  id={show.id}
+                  imageUrl={show.poster_path}
+                  title={show.original_name}
+                  rating={show.vote_average}
+                  year={
+                    show.first_air_date && show.first_air_date.substring(0, 4)
+                  }
+                />
               ))}
             </Section>
           )}
--- src/routes/TV/TVPresenter.js
+++ src/routes/TV/TVPresenter.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 import Loader from 'components/Loader';
 import Section from 'components/Section';
+import Poster from 'components/Poster';
 import Message from 'components/Message';
 
 const Container = styled.div`
@@ -24,21 +25,42 @@ export default function TVPresenter({
       {topRated && topRated.length > 0 && (
         <Section title='Top Rated Shows'>
           {topRated.map(show => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title='Popular Shows'>
           {popular.map(show => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title='Airing Today'>
           {airingToday.map(show => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
