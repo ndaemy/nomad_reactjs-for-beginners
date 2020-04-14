a1f168993a991a6c43a018c738190eb702f5a869
yuyaebean <yuyaebean@gmail.com>
2020-04-15 01:41:42 +0900

    6.1 HomePresenter and Section Components

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,19 +3,19 @@ import reset from 'styled-reset';
 
 const globalStyles = createGlobalStyle`
   ${reset}
-  a{
-    text-decoration: none;
-    color: inherit;
-  }
   * {
     box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
   }
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 12px;
     background-color: rgba(20, 20, 20, 1);
     color: white;
-    padding-top: 50px;
+    padding-top: 80px;
   }
 `;
 
--- src/routes/Home/HomePresenter.js
+++ src/routes/Home/HomePresenter.js
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import Section from 'components/Section';
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
 export default function HomePresenter({
   nowPlaying,
   upcoming,
@@ -9,7 +15,25 @@ export default function HomePresenter({
   loading,
   error,
 }) {
-  return <div></div>;
+  return loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title='Now Playing Movies'>
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title='Upcoming Movies'>
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title='Popular Movies'>
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 }
 
 HomePresenter.propTypes = {
