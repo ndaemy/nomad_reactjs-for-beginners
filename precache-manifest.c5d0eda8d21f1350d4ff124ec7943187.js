d4be7515e19c71f94ba03f44692c6a18b4b7cdac
yuyaebean <yuyaebean@gmail.com>
2020-04-17 19:53:53 +0900

    Updates

---
+++ precache-manifest.c5d0eda8d21f1350d4ff124ec7943187.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d4bc2c7be99cc3eb3b24ab3b14c769",
+    "url": "/nomad_reactjs-for-beginners/index.html"
+  },
+  {
+    "revision": "1ba619d5195057d9cb9b",
+    "url": "/nomad_reactjs-for-beginners/static/js/2.e56a4ed3.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/nomad_reactjs-for-beginners/static/js/2.e56a4ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b6c098efc393693d5d",
+    "url": "/nomad_reactjs-for-beginners/static/js/main.fa907afd.chunk.js"
+  },
+  {
+    "revision": "132bc56f093d4ee5d7e7",
+    "url": "/nomad_reactjs-for-beginners/static/js/runtime-main.935ddc3b.js"
+  }
+]);
